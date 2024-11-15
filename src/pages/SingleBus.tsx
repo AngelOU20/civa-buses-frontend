6f0f4568daf435af4015432b293a2fd016426c55
Julio Ucharima <julio_ucharima@usmp.pe>
2024-11-15 18:03:51 -0500

    update import path for getBusById in SingleBus component

--- src/pages/SingleBus.tsx
+++ src/pages/SingleBus.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { getBusById } from "@/services";
+import { getBusById } from "@/services/bus";
 import { Bus } from "@/interfaces";
 import { formatDate } from "@/utils/utils";
 import { BiArrowBack, BiBus } from "react-icons/bi";
