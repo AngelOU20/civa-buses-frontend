4b538675da75118a3ce837c9a5864f40b5491010
Julio Ucharima <julio_ucharima@usmp.pe>
2024-11-15 18:04:35 -0500

    add route for CreateBus component

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { Buses } from "@/pages/Buses";
 import { SingleBus } from "@/pages/SingleBus";
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "@/pages/ErrorPage";
+import { CreateBus } from "@/pages/CreateBus";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: "/bus/:id",
         element: <SingleBus />,
+      },
+      {
+        path: "/create-bus",
+        element: <CreateBus />,
       },
     ],
   },
